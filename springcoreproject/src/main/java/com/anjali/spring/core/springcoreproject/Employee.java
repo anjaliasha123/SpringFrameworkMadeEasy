dbd9e9f7017e18cfb5ef350b0334b9480d890faa
anjaliasha123 <anjaliasha95@gmail.com>
2023-08-09 10:18:03 -0500

    injecting collection types

--- springcoreproject/src/main/java/com/anjali/spring/core/springcoreproject/Employee.java
+++ springcoreproject/src/main/java/com/anjali/spring/core/springcoreproject/Employee.java
@@ -1,9 +1,22 @@
 package com.anjali.spring.core.springcoreproject;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.springframework.core.convert.Property;
+
 public class Employee {
 	
 	private int id;
 	private String name;
+	private List<String> departments;
+	private Set<String> managers;
+	private Map<String, String> deptRoleMap;
+	private Properties countryAndLanguages;
+	
+	
 	public int getId() {
 		return id;
 	}
@@ -16,5 +29,30 @@ public class Employee {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public List<String> getDepartments() {
+		return departments;
+	}
+	public void setDepartments(List<String> departments) {
+		this.departments = departments;
+	}
+	public Set<String> getManagers() {
+		return managers;
+	}
+	public void setManagers(Set<String> managers) {
+		this.managers = managers;
+	}
+	public Map<String, String> getDeptRoleMap() {
+		return deptRoleMap;
+	}
+	public void setDeptRoleMap(Map<String, String> deptRoleMap) {
+		this.deptRoleMap = deptRoleMap;
+	}
+	public Properties getCountryAndLanguages() {
+		return countryAndLanguages;
+	}
+	public void setCountryAndLanguages(Properties countryAndLanguages) {
+		this.countryAndLanguages = countryAndLanguages;
+	}
+	
 
 }
--- springcoreproject/src/main/java/com/anjali/spring/core/springcoreproject/Test.java
+++ springcoreproject/src/main/java/com/anjali/spring/core/springcoreproject/Test.java
@@ -1,6 +1,12 @@
 package com.anjali.spring.core.springcoreproject;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.convert.Property;
 
 public class Test {
 
@@ -8,6 +14,20 @@ public class Test {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/anjali/spring/core/springcoreproject/config.xml");
 		Employee e = (Employee)ctx.getBean("emp");
 		System.out.println("Employee name:"+e.getName()+" and id: "+e.getId());
+		List<String> dep = e.getDepartments();
+		for(String d: dep) {
+			System.out.println("Department name: "+d);
+		}
+		Set<String> managersName = e.getManagers();
+		for(String name: managersName) {
+			System.out.println("Manager: "+name);
+		}
+		Map<String, String> map = e.getDeptRoleMap();
+		for(String key: map.keySet()) {
+			System.out.println("key: "+key+" role: "+map.get(key));
+		}
+		Properties cl = e.getCountryAndLanguages();
+		System.out.println(cl);
 	}
 
 }
